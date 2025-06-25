685bd6aba10ab22ede9db8a77686b378a94412a4
Nandini Agarwal <nandini.qms@gmail.com>
2025-06-25 11:25:43 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class HelloDesignWorld {
+    public static void main(String[] args) {
+        String name = "Nandini";
+        String project = "Design Company";
+        
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Welcome to your Java-powered " + project + " toolbox.");
+        System.out.println("Let's create bold, playful, and minimal magic together!");
+    }
+}
